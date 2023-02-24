e6d1c6ee5c6e82f38b0281fe71a73b74ae2a0e69
fernan17893 <fernan17893@gmail.com>
2023-02-24 11:27:21 -0400

    it works, vegeta

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ tweetBtn = document.querySelector('.tweet');
 function randomQuote() {
     quoteBTN.classList.add('loading');
     quoteBTN.textContent = 'Loading...';
-    fetch('https://api.quotable.io/random')
+    fetch('https://animechan.vercel.app/api/random/character?name=vegeta')
     .then(res => res.json())
     .then(data => {
-        quoteText.textContent = data.content;
-        authorName.textContent = data.author;
+        quoteText.textContent = data.quote;
+        authorName.textContent = data.character;
         quoteBTN.textContent = 'New Quote';
         quoteBTN.classList.remove('loading');
     })
@@ -35,5 +35,11 @@ copyBtn.addEventListener('click', () => {
     alert('Quote copied to clipboard');
 });
 
-quoteBTN.addEventListener('click', randomQuote);
+function changeBackground() {
+    const colors = ['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#9e9e9e', '#607d8b'];
+    const randomColor = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomColor];
+}
 
+quoteBTN.addEventListener('click', randomQuote);
+quoteBTN.addEventListener('click', changeBackground);
